bfeab127d0855e37837ab289bd9ac01a142d9dfa
Yoshida1Shouyou <muslimovalisher76@gmail.com>
2025-12-04 19:29:07 +0700

    Update 11app.py

--- src/11app.py
+++ src/11app.py
@@ -1,6 +1,5 @@
-import random
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from services import calc
 
 app = Flask(
     __name__,
@@ -14,10 +13,34 @@ def index():
     return render_template("11home.html")
 
 
-@app.route("/CalcService/PassUsingJinja2")
-def about():
+@app.route("/calc", methods=["GET"])
+def calc_view():
+    return render_template("11calculater.html")
+
+
+@app.route("/calc", methods=["POST"])
+def calc_process_view():
+    first_number = request.form.get("first_number") or ""
+    second_number = request.form.get("second_number") or ""
+    operation = request.form.get("operation") or ""
+
+    try:
+        print(first_number, second_number, operation)
+        first_number = int(first_number)
+        second_number = int(second_number)
+        assert operation in ["+", "-", "*", "/"]
+    except (ValueError, AssertionError):
+        return render_template("11calculater.html")
+
+    try:
+        result = calc.calculate(first_number, second_number, operation)
+    except calc.ServiceError as e:
+        return render_template("11result.html", error=e.msg)
+
     return render_template(
-        "11calc.html",
-        first_num=random.randint(0, 10),
-        second_num=random.randint(0, 10),
+        "11result.html",
+        first_number=first_number,
+        second_number=second_number,
+        operation=operation,
+        result=result,
     )
